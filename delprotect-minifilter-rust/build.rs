5be2156a41c929f4008c59eed93f956fcc2fb5fa
rkumorek <rkumorek@cisco.com>
2023-04-21 12:04:26 +0200

    rewrite build.rs

--- delprotect-minifilter-rust/build.rs
+++ delprotect-minifilter-rust/build.rs
@@ -1,25 +1,21 @@
-/// copied from Matthias: https://github.com/not-matthias/kernel-driver-with-rust/blob/master/build.rs
-use failure::{format_err, Error};
-use std::{
-    env::var,
-    path::{Path, PathBuf},
-};
-use winreg::{enums::*, RegKey};
+use std::env::var;
+use std::path::{Path, PathBuf};
+use winreg::enums::HKEY_LOCAL_MACHINE;
+use winreg::RegKey;
 
-/// Returns the path to the `Windows Kits` directory. It's by default at
 /// `C:\Program Files (x86)\Windows Kits\10`.
-fn get_windows_kits_dir() -> Result<PathBuf, Error> {
+fn read_kits_path() -> PathBuf {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let key = r"SOFTWARE\Microsoft\Windows Kits\Installed Roots";
-    let dir: String = hklm.open_subkey(key)?.get_value("KitsRoot10")?;
+    let dir: String = hklm.open_subkey(key).unwrap().get_value("KitsRoot10").unwrap();
 
-    Ok(dir.into())
+    dir.into()
 }
 
 /// Returns the path to the kernel mode libraries. The path may look like this:
 /// `C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\km`.
-fn get_km_dir(windows_kits_dir: &PathBuf) -> Result<PathBuf, Error> {
-    let readdir = Path::new(windows_kits_dir).join("lib").read_dir()?;
+fn find_km_dir(windows_kits_dir: &PathBuf) -> PathBuf {
+    let readdir = Path::new(windows_kits_dir).join("lib").read_dir().unwrap();
 
     let max_libdir = readdir
         .filter_map(|dir| dir.ok())
@@ -32,42 +28,36 @@ fn get_km_dir(windows_kits_dir: &PathBuf) -> Result<PathBuf, Error> {
                 .unwrap_or(false)
         })
         .max()
-        .ok_or_else(|| format_err!("Can not find a valid km dir in `{:?}`", windows_kits_dir))?;
+        .unwrap();
 
-    Ok(max_libdir.join("km"))
+    max_libdir.join("km")
 }
 
-fn internal_link_search() {
-    let windows_kits_dir = get_windows_kits_dir().unwrap();
-    let km_dir = get_km_dir(&windows_kits_dir).unwrap();
+fn get_target_architecture() -> String {
     let target = var("TARGET").unwrap();
-
-    let arch = if target.contains("x86_64") {
+    if target.contains("x86_64") {
         "x64"
     } else if target.contains("i686") {
         "x86"
     } else {
-        panic!("Only support x86_64 and i686!");
-    };
-
-    let lib_dir = km_dir.join(arch);
-    println!(
-        "cargo:rustc-link-search=native={}",
-        lib_dir.to_str().unwrap()
-    );
+        panic!("Only support x86_64 and i686!")
+    }.to_string()
 }
 
-fn extra_link_search() {}
+
+fn get_kernel_library_dir() -> PathBuf {
+    let windows_kits_dir = read_kits_path();
+    let km_dir = find_km_dir(&windows_kits_dir);
+    let architecture = get_target_architecture();
+
+    km_dir.join(architecture)
+}
 
 fn main() {
-    if var(format!(
-        "CARGO_FEATURE_{}",
-        "extra_link_search".to_uppercase()
-    ))
-    .is_ok()
-    {
-        extra_link_search()
-    } else {
-        internal_link_search()
-    }
+
+    let km_dir = get_kernel_library_dir();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        km_dir.to_str().unwrap()
+    );
 }
